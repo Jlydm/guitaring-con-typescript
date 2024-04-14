a1086213032198d9463b4580d910b7ab1e1d9ee9
Jlydm <juliancito8790@gmail.com>
2024-04-14 00:21:06 -0500

    Finalizando...

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -34,28 +34,27 @@ export const cartReducer = (
 
   if(action.type === 'add-to-cart'){
 
-    const itemExists = state.cart.find((guitar) => guitar.id === action.payload.item.id)
+    const itemExists = state.cart.find(guitar => guitar.id === action.payload.item.id)
     let updateCart : CartItem[] = [] 
 
-    // Comprobar si ya existe o no
-    if(itemExists) {
     // Sumar un nuevo item
-    updateCart = state.cart.map(item => {
-      if(item.id === action.payload.item.id){
-        if(item.quantity < MAX_ITEMS){
-          return {...item, quantity: item.quantity + 1}
+    if(itemExists) {
+      updateCart = state.cart.map(item => {
+        if(item.id === action.payload.item.id){
+          if(item.quantity < MAX_ITEMS){
+            return {...item, quantity: item.quantity + 1}
+          } else {
+            return item
+          }
         } else {
           return item
         }
-      } else {
-        return item
-      }
-    })
-  } else {
-    // Agregar un nuevo item
-    const newItem : CartItem = {...action.payload.item, quantity : 1}
-    updateCart = [...state.cart, newItem]
-  }
+      })
+    } else {
+      // Agregar un nuevo item
+      const newItem : CartItem = {...action.payload.item, quantity : 1}
+      updateCart = [...state.cart, newItem]
+    }
     return {
       ...state,
       cart: updateCart
